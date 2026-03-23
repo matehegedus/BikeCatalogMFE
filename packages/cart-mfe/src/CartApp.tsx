6266e166b2bf5d608a79ae9baf850ac0537a074b
Mate Hegedus <matehegedus.mh@gmail.com>
2026-03-23 19:08:47 +0100

    added  CartMFE + useCart to connect react with our eventbus

--- packages/cart-mfe/src/CartApp.tsx
+++ packages/cart-mfe/src/CartApp.tsx
@@ -1,5 +1,56 @@
-import { Box } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  Stack,
+  Typography,
+} from "@mui/material";
+import { useCart } from "./hooks/useCart";
 
 export default function CartApp() {
-  return <Box>Carts are coming here:</Box>;
+  const cartItems = useCart();
+
+  return (
+    <Stack p={2}>
+      <Typography variant="h5">Your Cart:</Typography>
+
+      <Stack gap={2} direction={"row"} flexWrap={"wrap"}>
+        {cartItems.map((cartItem) => {
+          return (
+            <Card
+              key={cartItem.id}
+              variant="outlined"
+              sx={{
+                width: 400,
+              }}
+            >
+              <CardHeader title={cartItem.name} />
+              <CardContent>
+                <Stack direction="column">
+                  <Stack direction="row" alignItems="baseline">
+                    <Typography variant="h6">Amount:</Typography>
+                    <Typography variant="subtitle1">
+                      {cartItem.quantity}
+                    </Typography>
+                  </Stack>
+                  <Stack direction="row" alignItems="baseline">
+                    <Typography variant="h6">Price per item:</Typography>
+                    <Typography variant="subtitle1">
+                      {cartItem.price}
+                    </Typography>
+                  </Stack>
+                  <Stack direction="row" alignItems="baseline">
+                    <Typography variant="h6">Total:</Typography>
+                    <Typography variant="subtitle1">
+                      {cartItem.price * cartItem.quantity}
+                    </Typography>
+                  </Stack>
+                </Stack>
+              </CardContent>
+            </Card>
+          );
+        })}
+      </Stack>
+    </Stack>
+  );
 }
